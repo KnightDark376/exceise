71e75a44005d158c2cbea3a61e9bc86064ba268e
KnightDark376 <82499844+KnightDark376@users.noreply.github.com>
2021-06-07 23:27:58 +0700

    Add files via upload

---
+++ polygon.py
@@ -0,0 +1,16 @@
+class Polygon:
+    __width = 60
+    __height = 70
+
+    def set_values(self, width, height):
+        self.__width = width
+        self.__height = height
+    def get_width(self):
+        return self.__width
+    def get_height(self):
+        return self.__height
+
+
+
+
+
